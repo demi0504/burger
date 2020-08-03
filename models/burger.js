dc49742ee4967b76cb8f5cb681e3cb8fc3febede
demi0504 <ripley.demi@gmail.com>
2020-08-03 13:51:16 -0500

    router between orm functions and burger.js created

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,21 @@
 var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updataeOne: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+
 module.exports = burger;
