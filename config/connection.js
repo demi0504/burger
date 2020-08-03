4ac2b31face613c91e1a94f0e77948f30559d8ef
demi0504 <ripley.demi@gmail.com>
2020-08-03 13:41:28 -0500

    orm functions and connection created

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,20 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("conncected as id " + connection.threadId);
+});
+
+//export connection for ORM
 module.exports = connection;
