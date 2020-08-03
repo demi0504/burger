486277f80368c09b9c99f35414ba5eb910be9e23
demi0504 <ripley.demi@gmail.com>
2020-08-03 13:58:46 -0500

    fixed update function

--- models/burger.js
+++ models/burger.js
@@ -12,7 +12,7 @@ var burger = {
       });
     },
     updataeOne: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
         cb(res);
       });
     }
