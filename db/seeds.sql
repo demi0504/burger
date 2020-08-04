63c628de6f3954757f2a29175a96bf0fb6a450d0
demi0504 <ripley.demi@gmail.com>
2020-08-04 13:50:58 -0500

    devoured side handlebars in index

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,5 @@ USE burgers_db;
 INSERT INTO burgers (id, burger_name, devoured)
 VALUES 
 (1, "Cheeseburger", false),
-(2, "Black Bean Burger", TRUE),
-(3, "Cheesy Western", TRUE);
+(2, "Black Bean Burger", false),
+(3, "Cheesy Western", false);
