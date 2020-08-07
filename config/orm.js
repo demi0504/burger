3242087cb970a2b9b87881d2fce5a39445ffeef0
demi0504 <ripley.demi@gmail.com>
2020-08-07 14:37:21 -0500

    changed orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,50 +1,57 @@
 var connection = require("../config/connection.js");
 
-function objToSql(ob) {
+function printQuestionMarks(num) {
   var arr = [];
 
-  // loop through the keys and push the key/value as a string into arr
-  for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-          // if string with spaces, add quotations (Cheesy Western => 'Cheesy Western')
-          if (typeof value === "string" && value.indexOf(" ") >= 0) {
-              value = "'" + value + "'";
-          }
-          
-          arr.push(key + "=" + value);
-      }
+  for (var i = 0; i < num; i++) {
+      arr.push("?");
   }
 
   // translate array of strings to a single comma-separated string
   return arr.toString();
 }
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+        if(Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === 'string' && value.indexOf(" ") >= 0) {
+                value = "'" + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
+
 //selectAll, insertOne, updateOne orm functions
 var orm = {
-  selectAll: function(table, callback){
+  selectAll: function(table, cb){
       var queryString = "SELECT * FROM " + table + ";"
       connection.query(queryString, function(err, result) {
           if (err) throw err;
-          callback(result);
+          cb(result);
       });
   },
-  insertOne: function(table, cols, values, callback){
+  insertOne: function(table, cols, values, cb){
       var queryString = "INSERT INTO " + table;
       queryString += " (";
       queryString += cols.toString();
       queryString += ") ";
-      queryString += "VALUES (?";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
       queryString += ") ";
 
       connection.query(queryString, values, function(err, result) {
           if (err) {
               throw err;
           }
-          callback(result);
+          cb(result);
       });
   },
-  updateOne: function(table, objColVals, condition, callback) {
+  updateOne: function(table, objColVals, condition, cb) {
       console.log(objColVals)
       var queryString = "UPDATE " + table;
       queryString += " SET ";
@@ -57,7 +64,7 @@ var orm = {
           if (err) {
               throw err;
           }
-          callback(result);
+          cb(result);
       });
   },
 }
